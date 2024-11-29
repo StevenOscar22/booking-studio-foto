a568f054e9bb4f8ae16a5c6bc49f2180b6199164
StevenOscar22 <stevenoscar37@gmail.com>
2024-11-29 22:12:15 +0700

    view: edit home image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <header class="max-w-[100%] m-auto">
         <x-banner imgSrc="{{ asset('assets/banner_images/home_banner.png') }}" imgAlt="Banner Image"
             title="{{ $title }}">
-            <img src="{{ asset('assets/logo/kaia_logo_with_shadow.svg') }}" alt="">
+            <img src="{{ asset('assets/logo/kaia_logo_with_shadow.svg') }}" alt="Kaia Logo">
         </x-banner>
     </header>
 
